d9bde59a746467a1155ae65b622f94d7e2431bb6
Vortex <70918063+Vortttex@users.noreply.github.com>
2021-02-01 02:51:55 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.execute = async (client, message, args) => {
   
 message.channel.send(`Pong!`)
-
+}
 exports.conf = {
   command: "ping",
   description: "",
